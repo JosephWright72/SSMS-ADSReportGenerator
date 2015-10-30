d5a75e905a54e7c06b46c1ca14929f31df769aa6
JosephWright72 <joseph.wright.1972@gmail.com>
2015-10-30 16:29:14 +0000

    Draft of 1.3 - AAOS Timber Standard Door Sizes_Report Req UK_v1

--- FormFields.sql
+++ FormFields.sql
@@ -298,5 +298,933 @@ VALUES
 )
 GO
 
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_HW_SETS_0',
+	1,
+	NULL,
+	'A2',
+	'A2.40.NR/33dB',
+	NULL,
+	NULL,
+	0,
+	0
+)
+GO
 
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_HW_SETS_1',
+	1,
+	NULL,
+	'A3',
+	'A3.40.FD30S',
+	NULL,
+	NULL,
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_HW_SETS_2',
+	1,
+	NULL,
+	'A4',
+	'A4.40.FD30S/33dB',
+	NULL,
+	NULL,
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_HW_SETS_3',
+	1,
+	NULL,
+	'A5',
+	'A5.62.FD30S/38dB',
+	NULL,
+	NULL,
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_HW_SETS_4',
+	1,
+	NULL,
+	'A6',
+	'A6.62.FD30S/43dB',
+	NULL,
+	NULL,
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_HW_SETS_5',
+	1,
+	NULL,
+	'A7',
+	'A7.62.FD60S',
+	NULL,
+	NULL,
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_HW_SETS_6',
+	1,
+	NULL,
+	'A8',
+	'A8.62.FD60S/38dB',
+	NULL,
+	NULL,
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_0',
+	1,
+	NULL,
+	'SL7-1',
+	'7.700 x 2100.686 x 2089.625 x 2052',
+	'SL7-2',
+	'585.A1, A2, A3, A4, A5, A6, A7, A8.Single Leaf',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_1',
+	1,
+	NULL,
+	'SL8-1',
+	'8.800 x 2100.786 x 2089.725 x 2052',
+	'SL8-2',
+	'685.A1, A2, A3, A4, A5, A6, A7, A8.Single Leaf',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_2',
+	1,
+	NULL,
+	'SL9-1',
+	'9.900 x 2100.886 x 2089.825 x 2052',
+	'SL9-2',
+	'785.A1, A2, A3, A4, A5, A6, A7, A8.Single Leaf',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_3',
+	1,
+	NULL,
+	'SL10-1',
+	'10.1000 x 2100.986 x 2089.925 x 2052',
+	'SL10-2',
+	'885.A1, A2, A3, A4, A5, A6, A7, A8.Single Leaf',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_4',
+	1,
+	NULL,
+	'SL11-1',
+	'11.1100 x 2100.1086 x 2089.1025 x 2052',
+	'SL11-2',
+	'985.A1, A2, A3, A4, A5, A6, A7, A8.Single Leaf',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_5',
+	1,
+	NULL,
+	'SL12-1',
+	'12.1200 x 2100.1186 x 2089.1125 x 2052',
+	'SL12-2',
+	'1085.A1, A2.Single Leaf',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_6',
+	1,
+	NULL,
+	'SL13-1',
+	'13.1300 x 2100.1286 x 2089.1225 x 2052',
+	'SL13-2',
+	'1185.A1, A2.Single Leaf',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_7',
+	2,
+	NULL,
+	'ULP12-1',
+	'12.1200 x 2100.1186 x 2089.725 + 398 x 2052',
+	'ULP12-2',
+	'695 + 368.A1, A2, A3, A4, A5, A6, A7, A8.Unequal Leaf Pairs',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_8',
+	2,
+	NULL,
+	'ULP13-1',
+	'13.1300 x 2100.1286 x 2089.825 + 398 x 2052',
+	'ULP13-2',
+	'795 + 368.A1, A2, A3, A4, A5, A6, A7, A8.Unequal Leaf Pairs',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_9',
+	2,
+	NULL,
+	'ULP14-1',
+	'14.1400 x 2100.1386 x 2089.925 + 398 x 2052',
+	'ULP14-2',
+	'895 + 368.A1, A2, A3, A4, A5, A6, A7.Unequal Leaf Pairs',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_10',
+	2,
+	NULL,
+	'ULP15-1',
+	'15.1500 x 2100.1486 x 2089.925 + 398 x 2052',
+	'ULP12-2',
+	'895 + 468.A1, A2, A3, A4, A5, A6, A7, A8.Unequal Leaf Pairs',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_11',
+	2,
+	NULL,
+	'ULP16-1',
+	'16.1600 x 2100.1586 x 2089.925 + 398 x 2052',
+	'ULP16-2',
+	'895 + 568.A1, A2, A3, A4, A5, A6, A7, A8.Unequal Leaf Pairs',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_12',
+	2,
+	NULL,
+	'ULP17-1',
+	'17.1700 x 2100.1686 x 2089.925 + 698 x 2052',
+	'ULP17-2',
+	'895 + 668.A1, A2, A3, A4, A5, A6, A7, A8.Unequal Leaf Pairs',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_13',
+	2,
+	NULL,
+	'ULP18-1',
+	'18.1800 x 2100.1786 x 2089.925 + 798 x 2052',
+	'ULP18-2',
+	'895 + 768.A1, A2, A3, A4, A5, A6, A7, A8.Unequal Leaf Pairs',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_14',
+	2,
+	NULL,
+	'ULP19-1',
+	'19.1900 x 2100.1886 x 2089.925 + 898 x 2052',
+	'ULP19-2',
+	'895 + 868.A1, A2, A3, A4, A5, A6, A7, A8.Unequal Leaf Pairs',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_15',
+	3,
+	NULL,
+	'ELP13-1',
+	'13.1300 x 2100.1286 x 2089.611 + 611 x 2052',
+	'ELP13-2',
+	'2/581.A1, A2, A3, A4, A5, A6, A7, A8.Equal Leaf Pairs',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_16',
+	3,
+	NULL,
+	'ELP14-1',
+	'14.1400 x 2100.1386 x 2089.661 + 661 x 2052',
+	'ULP14-2',
+	'2/631.A1, A2, A3, A4, A5, A6, A7.Equal Leaf Pairs',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_17',
+	3,
+	NULL,
+	'ELP15-1',
+	'15.1500 x 2100.1486 x 2089.711 + 711 x 2052',
+	'ELP15-2',
+	'2/681.A1, A2, A3, A4, A5, A6, A7, A8.Equal Leaf Pairs',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_18',
+	3,
+	NULL,
+	'ELP16-1',
+	'16.1600 x 2100.1586 x 2089.761 + 761 x 2052',
+	'ELP16-2',
+	'2/731.A1, A2, A3, A4, A5, A6, A7, A8.Equal Leaf Pairs',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_19',
+	3,
+	NULL,
+	'ELP17-1',
+	'17.1700 x 2100.1686 x 2089.811 + 811 x 2052',
+	'ELP17-2',
+	'2/781.A1, A2, A3, A4, A5, A6, A7, A8.Equal Leaf Pairs',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_20',
+	3,
+	NULL,
+	'ELP18-1',
+	'18.1800 x 2100.1786 x 2089.861 + 861 x 2052',
+	'ULP18-2',
+	'2/831.A1, A2, A3, A4, A5, A6, A7, A8.Equal Leaf Pairs',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_21',
+	3,
+	NULL,
+	'ELP19-1',
+	'19.1900 x 2100.1886 x 2089.911 + 911 x 2052',
+	'ELP19-2',
+	'2/881.A1, A2, A3, A4, A5, A6, A7, A8.Equal Leaf Pairs',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_DOOR_SIZES_22',
+	3,
+	NULL,
+	'ELP20-1',
+	'20.2000 x 2100.1986 x 2089.961 + 961 x 2052',
+	'ELP20-2',
+	'2/931.A1, A2, A3, A4, A5, A6, A7.Equal Leaf Pairs',
+	0,
+	0
+)
+GO
+
+INSERT INTO AAOS_UAT_UK.dbo.FormFields
+(
+	Locale,
+	FormName,
+	ColName,
+	ColNumber,
+	VisibleColText,
+	Usage1Name,
+	Usage1Value,
+	Usage2Name,
+	Usage2Value,
+	DefaultWidth,
+	[Required]
+)
+VALUES
+(
+	'EN',
+	'rptEMEATimberStdDoorSizes',
+	'STD_FRAME_DEPTHS',
+	1,
+	NULL,
+	'STD_FRAME_DEPTHS',
+	'93, 96, 100, 110, 121, 128, 147, 150, 160, 170, 180, 200 and 250',
+	NULL,
+	NULL,
+	0,
+	0
+)
+GO
 
