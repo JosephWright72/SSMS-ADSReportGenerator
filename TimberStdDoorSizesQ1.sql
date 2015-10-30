afd4475787be62859d1c2b3806e5e94c564fe6b2
JosephWright72 <joseph.wright.1972@gmail.com>
2015-10-30 10:25:35 +0000

    Backed up project version

--- TimberStdDoorSizesQ1.sql
+++ TimberStdDoorSizesQ1.sql
@@ -1,2 +1,18 @@
-SELECT	PARSENAME(FF.Usage1Value, 3)  AS SetName, PARSENAME(FF.Usage1Value, 2)  AS Thickness, PARSENAME(FF.Usage1Value, 1)  AS Rating
-FROM FormFields FF WHERE	FF.FormName = 'rptEMEATimberStdDoorSizes' AND FF.ColName = 'HWSETS' ORDER BY FF.Usage1Name
+DECLARE @ProjectID INT
+SET @ProjectID = 28643
+SELECT	
+	AH.SetName,
+	AD.Thickness,
+	AD.FireRating + CASE WHEN ISNULL(AD.FireRating, '') = '' OR ISNULL(AD.AcousticRating, '') = '' THEN '' ELSE '/' END + AD.AcousticRating AS Rating,
+	AD.FireRating + '/' + AD.AcousticRating AS Rating
+FROM	AAOSProjects AP
+INNER JOIN AAOSDoors AD ON AP.id = AD.ProjectID
+INNER JOIN AAOSHWSets AH ON AP.id = AH.ProjectID AND AD.hwset = AH.setname
+WHERE AP.id = @ProjectID
+GROUP BY 
+	AH.SetName,
+	AD.Thickness,
+	AD.AcousticRating,
+	AD.FireRating
+ORDER BY 
+	AH.SetName
