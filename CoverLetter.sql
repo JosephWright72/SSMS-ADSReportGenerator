48190a349ed0907fa0aa439ded3738827d9bd151
JosephWright72 <joseph.wright.1972@gmail.com>
2015-12-07 09:35:26 +0000

    Revision number and format fixes

--- CoverLetter.sql
+++ CoverLetter.sql
@@ -1,9 +1,7 @@
 SELECT  
 	AP.id ProjectReference,
-	CASE WHEN ISNULL(AP.OriginalProjectID, 0) <> 0 
-         THEN CAST(AP.OriginalProjectID AS nvarchar(max))  +  '-'  +  CAST(AP.RevisionNumber AS nvarchar(max))
-         ELSE CAST(AP.ID AS nvarchar(max)) END as NewProjID,
-    CAST(AP.RevisionNumber AS nvarchar(max)) AS Revision,
+	CAST(AP.ID AS NVARCHAR(MAX)) AS ProjectReference,
+    CAST(AP.RevisionNumber AS NVARCHAR(MAX)) AS Revision,
 	AP.projectname ProjectName,
 	CASE WHEN DAY(getdate()) in (1,21,31) THEN convert(varchar,DAY(getdate())) + 'st ' WHEN DAY(getdate()) IN (2,22) then convert(varchar,DAY(getdate())) + 'nd ' WHEN DAY(getdate()) IN (3,23) then convert(varchar,DAY(getdate())) + 'rd '  ELSE convert(varchar,DAY(getdate())) + 'th ' end +  DATENAME(month, getdate())  + ' ' + convert(varchar,year(getdate())) TodaysDateEN, 
 	getdate() TodaysDate, 
@@ -32,5 +30,8 @@ left outer join AAOSConsultants CS on AP.consultant = CS.id
 left outer join AAOSFirms AA on AP.Architect = AA.id 
 left outer join AAOSFirms AC on AP.Contractor = AC.id 
 left outer join AAOSFirms AO on AP.Owner = AO.id 
-where AP.id = 28510 
-order by ap.id
+where isnull(AP.Address3,'') <>'' and revisionnumber>0 and isnull(ACS.Phone,'') <> '' 
+order by ap.id 
+
+select * from aaosconsultants where lastname ='Burrows'
+--update aaosconsultants set phone='(913) 948-6860' where lastname='Burrows'
